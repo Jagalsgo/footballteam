97b565afc5f37795dab319c2fa33a96dd2de30f3
Namix <ehdwns6781@naver.com>
2024-04-28 21:44:13 +0900

    git init

---
+++ src/test/java/com/footballteam/footballteam/FootballteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballteam.footballteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
